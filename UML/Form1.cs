d2f5a5cc4c4425defae42cd05c4e21e5009c3f61
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2019-04-28 21:03:31 +0200

    first commit

--- UML/Form1.cs
+++ UML/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace UML
 {
     public partial class Form1 : Form
@@ -15,6 +15,59 @@ namespace UML
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+
+            connection.ConnectionString =" Data Source =DESKTOP-NF0CFJ8\\SQLEXPRESS; Initial Catalog=UML; Integrated Security=True";
+            SqlCommand Command = new SqlCommand("Insert Into ACCOUNT values ( '" + textBox1.Text + "','" + textBox2.Text+"','"+textBox3.Text +"','"+textBox4.Text + "')", connection);
+
+            connection.Open();
+            MessageBox.Show(Command.CommandText);
+            Command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
