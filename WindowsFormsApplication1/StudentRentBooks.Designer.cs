bf9853004278aa5bbfe024e7c56cab72dd68bec2
Amr ali <lan_12_1999@hotmail.com>
2019-05-04 01:10:21 +0200

    Edited Book Browsing(student) to be Contains instead of Equal
    Added Student Renting Form
    
    Fixed User_ID to Student_ID to match Sign In form

--- WindowsFormsApplication1/StudentRentBooks.Designer.cs
+++ WindowsFormsApplication1/StudentRentBooks.Designer.cs
@@ -2,7 +2,7 @@
 
 namespace WindowsFormsApplication1
 {
-    partial class RentedBooks
+    partial class StudentRentBooks
     {
         /// <summary>
         /// Required designer variable.
@@ -31,6 +31,10 @@ namespace WindowsFormsApplication1
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SearchOptionList = new System.Windows.Forms.ComboBox();
+            this.SearchLabel = new System.Windows.Forms.Label();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
@@ -40,6 +44,10 @@ namespace WindowsFormsApplication1
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Controls.Add(this.SearchButton);
+            this.panel2.Controls.Add(this.SearchOptionList);
+            this.panel2.Controls.Add(this.SearchLabel);
+            this.panel2.Controls.Add(this.SearchTextBox);
             this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.ForeColor = System.Drawing.Color.Black;
             this.panel2.Location = new System.Drawing.Point(67, 39);
@@ -48,15 +56,57 @@ namespace WindowsFormsApplication1
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // SearchButton
+            // 
+            this.SearchButton.ForeColor = System.Drawing.Color.Black;
+            this.SearchButton.Location = new System.Drawing.Point(461, 63);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SearchButton.Size = new System.Drawing.Size(147, 30);
+            this.SearchButton.TabIndex = 23;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // SearchOptionList
+            // 
+            this.SearchOptionList.FormattingEnabled = true;
+            this.SearchOptionList.Location = new System.Drawing.Point(175, 38);
+            this.SearchOptionList.Name = "SearchOptionList";
+            this.SearchOptionList.Size = new System.Drawing.Size(177, 26);
+            this.SearchOptionList.TabIndex = 6;
+            this.SearchOptionList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.BackColor = System.Drawing.Color.Transparent;
+            this.SearchLabel.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.SearchLabel.Location = new System.Drawing.Point(62, 36);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(107, 28);
+            this.SearchLabel.TabIndex = 5;
+            this.SearchLabel.Text = "Search By:";
+            this.SearchLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.SearchLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(66, 70);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(286, 23);
+            this.SearchTextBox.TabIndex = 1;
+            this.SearchTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(-1, 57);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 146);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(635, 326);
+            this.dataGridView1.Size = new System.Drawing.Size(635, 268);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -70,7 +120,7 @@ namespace WindowsFormsApplication1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // RentedBooks
+            // StudentRentBooks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -86,13 +136,14 @@ namespace WindowsFormsApplication1
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "RentedBooks";
+            this.Name = "StudentRentBooks";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BrwoseBooks";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RentedBooks_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -108,6 +159,10 @@ namespace WindowsFormsApplication1
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.ComboBox SearchOptionList;
+        private System.Windows.Forms.Button SearchButton;
     }
 }
 
